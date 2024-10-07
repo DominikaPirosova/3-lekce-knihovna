62d250c99d260be086f7c9977663e2c27de2fb73
Dominika Pirosova <pirosova.dominika@gmail.com>
2024-10-07 20:17:18 +0200

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const kniha = {
+    nazov: "Blonde Roots",
+    autor: "Bernardine Evaristo",
+    strany: "262",
+    jazyk: "angličtina",
+    cena: 400,
+    naSklade: true,
+}
+
+
+document.body.innerHTML += "<p>Kniha: " + kniha.nazov + ", <br> Autor: " + kniha.autor + ", <br> Strany: " + kniha.strany + ", <br> Jazyk: " + kniha.jazyk + ", <br> Cena: " + kniha.cena + ", <br> Na Sklade: " + kniha.naSklade + "</p>"
